808f5bca80e90c5c95a4ad7c4623d9d199002c21
Maciejlys <maciej.lyszczarz98@gmail.com>
2021-12-09 22:30:43 +0100

    lvl 1 part 2

--- src/1/part-2/index.ts
+++ src/1/part-2/index.ts
@@ -1,9 +1,28 @@
 import { readAs } from "../../../utils/fileUtil";
 
-const input = readAs<string[]>({
-  parser: (input) => input,
-  path: "src/1/part-2/input-example",
-  splitter: /\r\n/,
+const input = readAs<number[]>({
+  parser: (input) => input.map((number) => parseInt(number)),
+  path: "src/1/part-1/input",
 });
 
-console.log(input);
+const increments = [];
+
+for (let index = 0; index < input.length; index++) {
+  if (index + 2 > input.length) break;
+  const sum = input[index] + input[index + 1] + input[index + 2];
+  if (!isNaN(sum)) {
+    increments.push(sum);
+  }
+}
+
+let sumOfIncrements = 0;
+
+for (let index = 1; index < increments.length; index++) {
+  const element = increments[index];
+  const prevElement = increments[index - 1];
+  if (element > prevElement) {
+    sumOfIncrements++;
+  }
+}
+
+console.log(sumOfIncrements);
