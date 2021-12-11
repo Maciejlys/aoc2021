d38b5f61f77b1c683c714d5c5f786e0d6558d441
Maciejlys <maciej.lyszczarz98@gmail.com>
2021-12-11 18:06:49 +0100

    moved Day 4 Part 1

--- src/4/index.ts
+++ src/4/index.ts
@@ -1,22 +1,73 @@
 import { readAs } from "../utils/fileUtil";
 import { transposeMatrix, arrayInArrayFilled } from "../utils/arrayUtils";
 
-const input = readAs<string[]>({
-  parser: (input) => input,
-  path: "src/4/input-example",
-  splitter: /\r\n/,
-});
-
-interface Board {
-  id: number;
+class Board {
   values: number[][];
   matches: boolean[][];
   winner: boolean;
+
+  constructor(values: number[][], matches: boolean[][], winner: boolean) {
+    this.values = values;
+    this.matches = matches;
+    this.winner = winner;
+  }
+
+  log() {
+    console.log(this.winner, this.values, this.matches);
+  }
+
+  mark(chosen: number) {
+    for (let columnIndex = 0; columnIndex < this.values.length; columnIndex++) {
+      const row = this.values[columnIndex];
+      if (row.includes(chosen)) {
+        let index = row.indexOf(chosen);
+        this.matches[columnIndex][index] = true;
+      }
+    }
+  }
+
+  checkWin(): boolean {
+    const normal = this.matches;
+    const transposed = transposeMatrix(this.matches);
+    for (let index = 0; index < normal.length; index++) {
+      if (normal[index].every((x) => x === true)) {
+        this.winner = true;
+        return true;
+      } else if (transposed[index].every((x) => x === true)) {
+        this.winner = true;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  sumOfUnmarked(): number {
+    const result: number[] = [];
+
+    for (let index = 0; index < this.values.length; index++) {
+      const element = this.values[index];
+      for (
+        let elementIndex = 0;
+        elementIndex < element.length;
+        elementIndex++
+      ) {
+        const e = element[elementIndex];
+        if (!this.matches[index][elementIndex]) result.push(e);
+      }
+    }
+    return result.reduce((a, b) => a + b, 0);
+  }
 }
 
-let allBoards: Board[] = [];
+const allBoards: Board[] = [];
 
-const chosen = input[0].split(",").map((x) => parseInt(x));
+const input = readAs<string[]>({
+  parser: (input) => input,
+  path: "src/4/input",
+  splitter: /\r\n/,
+});
+
+const chosenNumbers = input[0].split(",").map((x) => parseInt(x));
 const boards = input
   .splice(1, input.length - 1)
   .map((x) =>
@@ -28,80 +79,55 @@ const boards = input
   .filter((x) => x.length > 0);
 
 for (let index = 0; index < boards.length; index += 5) {
-  allBoards.push({
-    id: index,
-    values: [
-      boards[index],
-      boards[index + 1],
-      boards[index + 2],
-      boards[index + 3],
-      boards[index + 4],
-    ],
-    matches: arrayInArrayFilled(5, false),
-    winner: false,
-  });
+  allBoards.push(
+    new Board(
+      [
+        boards[index],
+        boards[index + 1],
+        boards[index + 2],
+        boards[index + 3],
+        boards[index + 4],
+      ],
+      arrayInArrayFilled(5, false),
+      false
+    )
+  );
 }
 
-const checkForWin = (arr: boolean[][]): boolean => {
-  const tranposedArr = transposeMatrix(arr);
-  for (let index = 0; index < arr.length; index++) {
-    const row = arr[index];
-    const transRow = tranposedArr[index];
-    if (row.every((x) => x === true || transRow.every((x) => x === true))) {
-      return true;
+const part1 = () => {
+  let winners = [];
+
+  for (let chosenIndex = 0; chosenIndex < chosenNumbers.length; chosenIndex++) {
+    const chosen = chosenNumbers[chosenIndex];
+    let loserBoards = allBoards.filter((x) => x.winner === false);
+    for (let index = 0; index < loserBoards.length; index++) {
+      const board = loserBoards[index];
+      board.mark(chosen);
+      if (board.checkWin()) winners.push([board, chosen]);
     }
   }
-  return false;
+  const [lastWinner, chosen] = winners[0];
+  const sum = (lastWinner as Board).sumOfUnmarked();
+  console.log((chosen as number) * sum);
 };
 
-// Part 1
+const part2 = () => {
+  let winners = [];
 
-const markAllChosen = (): number[] => {
-  let winner = -1;
-  for (let index = 0; index < chosen.length; index++) {
-    const chosenNumber = chosen[index];
-    for (
-      let allBoardsIndex = 0;
-      allBoardsIndex < allBoards.length;
-      allBoardsIndex++
-    ) {
-      const singleBoard = allBoards[allBoardsIndex];
-      for (
-        let singleBoardIndex = 0;
-        singleBoardIndex < singleBoard.values.length;
-        singleBoardIndex++
-      ) {
-        const rowValues = singleBoard.values[singleBoardIndex];
-        if (rowValues.includes(chosenNumber)) {
-          let indexOfElement = rowValues.indexOf(chosenNumber);
-          singleBoard.matches[singleBoardIndex][indexOfElement] = true;
-        }
-      }
-      if (checkForWin(singleBoard.matches)) {
-        return [singleBoard.id, chosenNumber];
-      }
+  for (let chosenIndex = 0; chosenIndex < chosenNumbers.length; chosenIndex++) {
+    const chosen = chosenNumbers[chosenIndex];
+    let loserBoards = allBoards.filter((x) => x.winner === false);
+    for (let index = 0; index < loserBoards.length; index++) {
+      const board = loserBoards[index];
+      board.mark(chosen);
+      if (board.checkWin()) winners.push([board, chosen]);
     }
   }
 
-  return [winner];
+  const [lastWinner, chosen] = winners[winners.length - 1];
+  const sum = (lastWinner as Board).sumOfUnmarked();
+  console.log((chosen as number) * sum);
 };
 
-const getUnmarkedSum = (board: Board): number => {
-  const result: number[] = [];
-
-  for (let index = 0; index < board.values.length; index++) {
-    const element = board.values[index];
-    for (let elementIndex = 0; elementIndex < element.length; elementIndex++) {
-      const e = element[elementIndex];
-      if (!board.matches[index][elementIndex]) result.push(e);
-    }
-  }
-  return result.reduce((a, b) => a + b, 0);
-};
-
-const [id, number] = markAllChosen();
-const index = id / 5;
-
-const sumOfUnmarked = getUnmarkedSum(allBoards[index]);
-
-console.log(sumOfUnmarked * number);
+// part1();
+// part2();
