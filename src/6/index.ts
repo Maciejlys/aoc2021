a72078153dd1ca20fbfe943f3149fb855b14cac0
Maciejlys <maciej.lyszczarz98@gmail.com>
2021-12-13 01:16:01 +0100

    Day 6 Part 2 solved

--- src/6/index.ts
+++ src/6/index.ts
@@ -6,15 +6,57 @@ const input = readAs<string[]>({
   splitter: /\r\n/,
 });
 
+interface CachedFishes {
+  [key: number]: number;
+}
+
 class Pond {
-  currentPopulation: Lanternfish[];
+  //part1
+  private _currentPopulation: Lanternfish[];
+
+  //part2
+  private _cachedPopulation: CachedFishes;
 
   constructor(initialPopulation: Lanternfish[]) {
-    this.currentPopulation = initialPopulation;
+    this._currentPopulation = initialPopulation;
+
+    this._cachedPopulation = [];
+    this.setInitial();
+  }
+
+  private setInitial() {
+    this._cachedPopulation = this.getEmptyCache();
+    for (const fish of initialPopulation) {
+      this._cachedPopulation[fish.spawnTime] += 1;
+    }
+  }
+
+  getNumberOfFishes() {
+    return Object.values(this._cachedPopulation).reduce((a, b) => a + b);
+  }
+
+  getCached() {
+    return this._cachedPopulation;
+  }
+
+  getToDayWithCached(dayNumber: number) {
+    for (let index = 0; index < dayNumber; index++) {
+      const empty = this.getEmptyCache();
+
+      for (let internalIndex = 0; internalIndex < 9; internalIndex++) {
+        if (internalIndex === 0) {
+          empty[8] += this._cachedPopulation[0];
+          empty[6] += this._cachedPopulation[0];
+        } else {
+          empty[internalIndex - 1] += this._cachedPopulation[internalIndex];
+        }
+      }
+      this._cachedPopulation = { ...empty };
+    }
   }
 
   getCurrentPopulation() {
-    return this.currentPopulation;
+    return this._currentPopulation;
   }
 
   skipToDay(dayNumber: number) {
@@ -23,8 +65,16 @@ class Pond {
     }
   }
 
+  private getEmptyCache = () => {
+    const cache: CachedFishes = [];
+    for (let index = 0; index < 9; index++) {
+      cache[index] = 0;
+    }
+    return cache;
+  };
+
   goToDayAnother() {
-    let previousDay = this.currentPopulation;
+    let previousDay = this._currentPopulation;
     let currentPopulation: Lanternfish[] = [];
 
     previousDay.forEach((fish) => {
@@ -38,7 +88,7 @@ class Pond {
       }
     });
 
-    this.currentPopulation = currentPopulation;
+    this._currentPopulation = currentPopulation;
   }
 }
 
@@ -70,4 +120,12 @@ const part1 = () => {
   console.log(result);
 };
 
-part1();
+const part2 = () => {
+  const pond = new Pond(initialPopulation);
+  pond.getToDayWithCached(256);
+  const result = pond.getNumberOfFishes();
+  console.log(result);
+};
+
+// part1();
+part2();
